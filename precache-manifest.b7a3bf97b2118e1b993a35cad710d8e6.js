02491b8ca5c31f44ee4beafe7c32cf2af72329be
Golam Rabbani <mgorabbani@gmail.com>
2021-08-14 21:42:08 +0200

    Updates

---
+++ precache-manifest.b7a3bf97b2118e1b993a35cad710d8e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bde1b60187f99ad0e3943bf4ba71f18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5f7ce936fd3c6f3d4f3",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "ffdac39d7c78bff6847b",
+    "url": "./static/js/2.f7548748.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f7548748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5f7ce936fd3c6f3d4f3",
+    "url": "./static/js/main.1aeccebe.chunk.js"
+  },
+  {
+    "revision": "07299f286304423184f0",
+    "url": "./static/js/runtime-main.802452e3.js"
+  }
+]);
